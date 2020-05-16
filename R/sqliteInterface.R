682da5f6d1eb526bb6f205cd97c0a10e609b4c5a
sivanMargalit <margalit.sivan@gmail.com>
2020-05-16 14:46:45 +0300

    change exportForKamadata - so now it can generate sqlite for Kamadata from scratch. Does not need template empty sqlite anymore

--- R/sqliteInterface.R
+++ R/sqliteInterface.R
@@ -10,6 +10,8 @@
 #' @examples
 #' loadFromSQLite(sqliteFieName)
 #'
+#' @import RSQLite
+#'
 #' @export
 loadFromSQLite<- function(dbn, tbl="localizations")
 {
@@ -31,6 +33,8 @@ loadFromSQLite<- function(dbn, tbl="localizations")
 #'
 #' @examples
 #' saveIntoSQLite(<sqliteFieName>)
+#'
+#' @import RSQLite
 #'
 #' @export
 saveIntoSQLite<- function(dbn,tbls,append=FALSE)
@@ -116,6 +120,7 @@ sqliteGetQuery<-function(dbn,aQuery){
 #' @examples
 #' exportForKamadata(my.df, "kamadata.sqlite")
 #'
+#'
 #' @export
 exportForKamadata<-function(loc.df, sqliteName, append=TRUE){
   if(!file.exists(sqliteName)){
@@ -127,4 +132,157 @@ exportForKamadata<-function(loc.df, sqliteName, append=TRUE){
     saveIntoSQLite(sqliteName,list("LOCALIZATIONS"=loc.df),append = append)
   }
 
+}
+
+
+#' generate sqlite file in atlas format  for KAMADATA
+#'
+#' It update the localization table with the data in loc.df data frame
+#'
+#'
+#'
+#' @param loc.df input data frame should include the fields: TAG (number up to 10 digits),
+#' TIME (13 digit) - epoch time in milisec, recomended in UTC Timezone
+#' X, Y (double) - coordinates in ITM marctor
+#' @param sqliteName sqlite file name with atlas table
+#' @param append default FALSE. means do not append data on previous data
+#' @param overwrite default FALSE. means co not overwrite if file exists.  If overwrite parameter is TRUE than append ignored
+#' @param atlas.system default 'hula'.
+#'
+#'
+#' @examples
+#' exportForKamadata("kamadata.sqlite", my.df, append=TRUE, atlas.system="harod")
+#'
+#' @import DBI
+#' @import RSQLite
+#'
+#' @export
+exportForKamadata<- function(sqliteName,
+                             loc.df=NULL,
+                             append=FALSE,
+                             overwrite=FALSE,
+                             atlas.system="hula"){
+
+  if (file.exists(sqliteName)){
+    if ((append==FALSE) & (overwrite==FALSE)){
+      print(sprintf("File %s already exists and append=FALSE & overwrite=FALSE. please choose different file name or update write permissions",
+                    sqliteName))
+      return(0)
+    }
+  }
+
+  # connect to the sqlite file - if not exists it create empty file
+  con = RSQLite::dbConnect(RSQLite::SQLite(),dbname=sqliteName)
+
+  #if atlas table not exists - create tables
+  tbls<-dbListTables(con)
+  if (length(tbls)==0){
+    con<-generateAtlasEmptySQlite(con,atlas.system)
+    append<-TRUE
+  }
+  else if (overwrite == TRUE){
+    print(sprintf("File %s already exists but overwrite permission will delete previous data",
+                  sqliteName))
+    con<-cleanAtlasData(con)
+    append<-TRUE
+  }
+
+  RSQLite::dbDisconnect(con)
+  #unlink(sqliteName)
+
+
+  if (is.null(loc.df)){
+    print(sprintf("File %s has now empty ATLAS tables",
+                  sqliteName))
+  }else{
+    saveIntoSQLite(sqliteName,
+                   list("LOCALIZATIONS"=loc.df),
+                   append = append)
+  }
+}
+
+
+generateAtlasEmptySQlite<-function(conn, atlas.system="hula"){
+  rslt<-dbExecute(conn,
+                  "CREATE TABLE LOCALIZATIONS(
+                TAG           bigint(20) NOT NULL,
+                TX            bigint(20) DEFAULT NULL,
+                TIME          bigint(20) NOT NULL,
+                X             double     DEFAULT NULL,
+                Y             double     DEFAULT NULL,
+                Z             double     DEFAULT NULL,
+                FREQ          double  DEFAULT NULL,
+                DIM           int(11) DEFAULT NULL,
+                NBS           int(11) DEFAULT NULL,
+                NCONSTRAINTS  int(11) DEFAULT NULL,
+                PENALTY       float   DEFAULT NULL,
+                GRADNRM       float   DEFAULT NULL,
+                VARX          float   DEFAULT NULL,
+                VARY          float   DEFAULT NULL,
+                VARZ          float   DEFAULT NULL,
+                COVXY         float   DEFAULT NULL,
+                COVXZ         float   DEFAULT NULL,
+                COVYZ         float   DEFAULT NULL,
+                PRIMARY KEY   (TAG,TIME)
+            );")
+  #dbFetch(rs)
+  #dbClearResult(rs)
+  rslt<-dbExecute(conn,
+                  "CREATE TABLE DETECTIONS (
+                BS          int(11)      NOT NULL,
+                TAG         bigint(20)   NOT NULL,
+                TX          bigint(20)   NOT NULL,
+                TIME        bigint(20)    NOT NULL,
+                SAMPLES_CLK double       NOT NULL,
+                SNR         float        DEFAULT NULL,
+                RSSI        float        DEFAULT NULL,
+                HEADROOM    float        DEFAULT NULL,
+                GAIN        float        DEFAULT NULL,
+                PRIMARY KEY (TAG, BS, TIME)
+              );")
+  rslt<-dbExecute(conn,
+                  "CREATE TABLE GPS_LOCALIZATIONS (
+                ID            bigint(20) NOT NULL,
+                TIME          bigint(20) NOT NULL,
+                LAT           double     DEFAULT NULL,
+                LON           double     DEFAULT NULL,
+                ASL           float      DEFAULT NULL,
+                HDOP          float      DEFAULT NULL,
+                PRIMARY KEY (ID, TIME)
+              );")
+
+  rslt<-dbExecute(conn,
+                  "CREATE TABLE PROPERTIES (
+                KEY          VARCHAR      NOT NULL,
+                VALUE        VARCHAR   NOT NULL,
+                PRIMARY KEY (KEY)
+              );")
+
+  rslt<-dbExecute(conn,
+                  "CREATE INDEX DET_TIME_IDX ON DETECTIONS (TIME);")
+
+  rslt<-dbExecute(conn,
+                  "CREATE INDEX LOC_TIME_IDX ON LOCALIZATIONS (TIME);")
+
+  rslt<-dbExecute(conn,
+                  "CREATE INDEX GPS_TIME_IDX ON GPS_LOCALIZATIONS (TIME) ;")
+
+
+  dbWriteTable(conn,
+               "PROPERTIES",
+               data.frame("KEY"=c("atlas-system"),
+                          "VALUE"=c(atlas.system)),
+               append=T,
+               row.names = F)
+  return(conn)
+}
+
+cleanAtlasData<-function(conn){
+  rslt<-dbExecute(conn,
+                  "delete from LOCALIZATIONS;")
+  rslt<-dbExecute(conn,
+                  "delete from DETECTIONS;")
+  rslt<-dbExecute(conn,
+                  "delete from  GPS_LOCALIZATIONS;")
+  return(conn)
 }
